e06f107bbb135b934062da52bfee45b4c4d3ff46
Josias Carneiro <104440548+josiascta@users.noreply.github.com>
2024-11-19 22:59:33 -0300

    Add files via upload

---
+++ src/main/java/br/com/ifpb/ads/bookifyapi/BookifyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.ads.bookifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpb/ads/bookifyapi/BookifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.ads.bookifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
